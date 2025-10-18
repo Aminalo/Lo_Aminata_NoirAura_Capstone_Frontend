ca2e469d9a023fbc2386fa4e49c65c0e42cde0e9
Amina <107080579+Aminalo@users.noreply.github.com>
2025-10-18 14:04:52 -0400

    add app routes with protected salons page and default redirects

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { Routes, Route, Navigate } from "react-router-dom";
+import { useAuth } from "./contexts/authContext/authContext";
 
-function App() {
-  const [count, setCount] = useState(0)
+// Pages & Components
+import AuthPage from "./pages/auth/index";
+import SalonList from "./pages/salons/index";
+import NotFound from "./pages/NotFound";
+import Navbar from "./components/navbar/index";
+import ProtectedRoutes from "./components/protected_routes/ProtectedRoutes";
+
+export default function App() {
+  const { cookies } = useAuth();
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+      <Navbar />
+      <Routes>
+        {/* Default route */}
+        <Route
+          path="/"
+          element={
+            cookies.token ? (
+              <Navigate to="/salons" replace />
+            ) : (
+              <Navigate to="/auth" replace />
+            )
+          }
+        />
 
-export default App
+        {/* Auth */}
+        <Route path="/auth" element={<AuthPage />} />
+
+        {/* Private pages */}
+        <Route element={<ProtectedRoutes />}>
+          <Route path="/salons" element={<SalonList />} />
+        </Route>
+
+        {/* 404 */}
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </>
+  );
+}
